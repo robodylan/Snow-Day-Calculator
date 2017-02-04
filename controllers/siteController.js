0884b966f3faebd79218e69da8d04f5e24d94e9f
Dylan Dunn <robodylan123@gmail.com>
2017-02-03 20:58:11 -0500

    Finished GUI

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -3,52 +3,47 @@
  */
 var request = require('request');
 var sleep = require('thread-sleep');
+var exphbs = require('express-handlebars');
 
 module.exports.home = function(req,res) {
-    res.send("I'm from Home");
+    res.render('home');
 }
 
 module.exports.api = function(req,res) {
     var currentTime = new Date();
+    var calculateWithDataFrom;
     //It's still the morning, calculate based  for today
-    if(currentTime.getHours() < 7) {
+    if(currentTime.getHours() != 25) {
+      sleep(2000);
+      request('http://api.wunderground.com/api/e1e738109f12d10c/forecast/q/' + req.params.location + '.json', function(error, response, body) {
+          var snowDays = req.params.days;
+          var tomorrowForecast;
+          tomorrowForecast = JSON.parse(body).forecast.simpleforecast.forecastday[1];
+          var snowfall = tomorrowForecast.snow_day.cm;
+          var temp = tomorrowForecast.low.fahrenheit - 10;
+          var windSpeed = tomorrowForecast.maxwind.mph;
+
+          var snowFactor = snowfall / 10;
+          if (temp < 0)
+          {
+              tempFactor = .5 + (Math.abs(temp) / -50);
+          } else {
+              tempFactor = .5;
+          }
+
+          otherFactor = .5 + (windSpeed / 50);
+
+          var finalPercentage = ((snowFactor + tempFactor + otherFactor) / 3) - (Math.pow(snowDays, 2) / 100);
+          if (finalPercentage > 1) {
+              finalPercentage = 1;
+          }
+
+          if(finalPercentage < 0) {
+            finalPercentage = 0;
+          }
+          res.send({ "Percentage" : Math.round(finalPercentage * 98)});
+      });
+    }else {
 
     }
-    //It's passed cancellation time, calculate for tomorrow
-    if(currentTime.getHours() > 7) {
-
-    }
-    //controls are the exponential component of each control's factor
-    //get weather data and set variables
-    sleep(2000);
-    request('http://api.wunderground.com/api/e1e738109f12d10c/forecast/q/13114.json', function(error, response, body) {
-        var snowDays = 4;
-        var tomorrowForecast;
-        tomorrowForecast = JSON.parse(body).forecast.simpleforecast.forecastday[1];
-        var snowfall = tomorrowForecast.snow_day.cm;
-        var temp = tomorrowForecast.low.fahrenheit;
-        temp = 10;
-        var windSpeed = (tomorrowForecast.maxwind.mph + tomorrowForecast.maxwind.mph) / 2;
-
-        var snowFactor = ((snowfall / 14) ^ 2) * 100; //Snowfall out of 60cm, 60cm = 100% chance, exponential
-        if (temp < 0) //If temp is below zero, better exponential chance of snowday
-        {
-            tempFactor = (((temp / -25) ^ 2) * 50);
-        } else {
-            tempFactor = 0;
-        }
-
-        otherFactor = ((windSpeed / 50) * 25) + (25 / snowDays);
-
-        var finalPercentage = (snowFactor + tempFactor + otherFactor) / 300;
-
-        if (finalPercentage > 1) {
-            finalPercentage = 1;
-            console.log(finalPercentage);
-        }
-
-        res.send({ "Percentage" : Math.round(finalPercentage * 100) });
-    });
 }
-
-
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var handlebars = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var request  =  require('request');
 var siteController = require('./controllers/siteController.js');
-
 var app = express();
-app.engine('handlebars', handlebars({defaultlayout: 'main'}));
-app.set('view engine', handlebars)
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', siteController.home);
 
-app.get('/api/:location', siteController.api);
+app.get('/api/:location/:days', siteController.api);
 
 app.listen(3000, function() {
   console.log("Express started");
