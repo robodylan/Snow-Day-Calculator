e9912b72411ebbf25444c60240a650897ed1b68c
Dylan Dunn <robodylan123@gmail.com>
2017-02-05 15:36:41 -0500

    Removed Result Bug
    Removed bug where result returns as undefined if input is invalid. Added
    error handing and reporting to user

--- public/js/snow-calc.js
+++ public/js/snow-calc.js
@@ -1,12 +1,19 @@
 function calculate() {
   var zipcode = $('#zipcode').val();
+  if($('#days').val() == "") {
+    $('#days').val(0);
+  }
   var days = $('#days').val();
   var url = '/api/' + zipcode + '/' + days + '/';
   $('#result-msg').html("Getting the lastest info...");
   $.ajax({
     url: url
   }).then(function(data){
-    $('#result-msg').html("The chance you will have a snow day tomorrow is " + data.Percentage + "%");
+    if(data.hasOwnProperty('Percentage') && data.Percentage != "%N/A") {
+      $('#result-msg').html("The chance you will have a snow day tomorrow is " + data.Percentage + "%");
+    }else {
+      $('#result-msg').html("Error Processing Request: Invalid Zipcode");
+    }
   });
 
 }
